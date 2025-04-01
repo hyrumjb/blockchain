0f46c9676f3fa3cdd9a08e353d4d0bae9373321f
Hyrum Bradshaw <bradshaw.hyrum@gmail.com>
2025-03-31 19:49:10 -0600

    added app.py file

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, jsonify
+import hashlib
+import datetime as date
+
+
+app = Flask(__name__)
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        hash_string = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        return hashlib.sha256(hash_string.encode()).hexdigest()
+        
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, date.datetime.now(), "Genesis Block", "0")
+    
+    def get_latest_block(self):
+        return self.chain[-1]
+    
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            
+            if current_block.previous_hash != previous_block.hash:
+                return False
+            
+        return True
+    
+
+blockchain = Blockchain()
+blockchain.add_block(Block(1, date.datetime.now(), "Transaction Data 1", ""))
+blockchain.add_block(Block(2, date.datetime.now(), "Transaction Data 2", ""))
+
+
+def get_blockchain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append({
+            'index': block.index,
+            'timestamp': str(block.timestamp),
+            'data': block.data,
+            'hash': block.hash,
+            'previous_hash': block.previous_hash
+        })
+    return jsonify(chain_data)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
